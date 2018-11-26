9ca6c36680ca49f1f36fab00fda160b8d770b1bd
BMNTeam <maksim_bender08@mail.ru>
2018-11-26 20:59:01 +0300

    Admin page has been initialized

---
+++ src/app/components/main/admin/AdminComponent.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+
+class AdminComponent extends React.Component {
+    public render()
+    {
+        return (
+            <div>
+                {/*<h6>Введите данные отчета</h6>*/}
+                <br/><br/>
+                <form action="">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="exampleFormControlInput1">Название отчета</label>
+                                <input type="email" className="form-control" id="exampleFormControlInput1"
+                                       placeholder=""/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="">Файл отчета</label>
+                                <div className="custom-file">
+                                    <input type="file" className="custom-file-input" id="validatedCustomFile" />
+                                    <label className="custom-file-label" htmlFor="validatedCustomFile">Выберите файл</label>
+                                    <div className="invalid-feedback">Example invalid custom file feedback</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br/>
+                    <button type="submit" className="btn btn-primary">Отправить</button>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default AdminComponent;
