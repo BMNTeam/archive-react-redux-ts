1a8e4e2e12714efd8520a4c447ec18ab8bac0a80
BMNTeam <maksim_bender08@mail.ru>
2019-01-01 16:54:52 +0300

    Dropdown has been added to header component

---
+++ src/app/structure/header/DropdownAddMenu.tsx
@@ -0,0 +1,52 @@
+import {RefObject} from "react";
+import * as React from "react";
+import {Link} from "react-router-dom";
+
+export class DropdownMenu extends React.Component<{ toggleDropdown: (v?: boolean) => void}, any>
+{
+    private node: RefObject<HTMLDivElement>;
+
+    constructor(props: { toggleDropdown: (v?: boolean) => void})
+    {
+        super(props);
+        this.node = React.createRef<HTMLDivElement>();
+        this.handleClick  = this.handleClick.bind(this);
+
+    }
+    public componentWillMount(): void
+    {
+        document.addEventListener('click', this.handleClick, false);
+    }
+
+    public componentWillUnmount()
+    {
+        document.removeEventListener('click', this.handleClick, false);
+    }
+
+
+    public handleClick(e: MouseEvent)
+    {
+
+        if(this.node.current!.contains(e.target as Node) && !(e.target instanceof HTMLAnchorElement))
+        {
+
+            return;
+        }
+
+        this.props.toggleDropdown(false);
+    }
+
+    public render()
+    {
+        return (
+            <div ref={this.node} className='dropdown-menu show' aria-labelledby="navbarDropdown">
+                <Link to="/admin" className="dropdown-item">Отчет</Link>
+                <Link to="/admin" className="dropdown-item">Ссылку</Link>
+                <div className="dropdown-divider" />
+                <Link to="/admin" className="dropdown-item">Сотрудника</Link>
+                <Link to="/admin" className="dropdown-item">Статью</Link>
+
+            </div>
+        )
+    }
+}
