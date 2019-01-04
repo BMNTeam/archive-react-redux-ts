6e7ce8d74eff3f3430040865dc590b568c817da4
BMNTeam <maksim_bender08@mail.ru>
2019-01-04 18:54:41 +0300

    Getting options functionality has been moved to a separate class

---
+++ src/app/structure/main/admin/admin-service.ts
@@ -0,0 +1,31 @@
+import axious from "axios";
+import {env} from "../../../env";
+
+export class AdminService
+{
+   public async getEmployeesOptions()
+   {
+       const employees = await this.getData<Models.IEmployee>(env.endpoints.employees);
+       return employees.map(e => this.getOptions(e.id, e.full_name))
+   }
+
+   public async getJournalsOptions()
+   {
+       const journals = await this.getData<Models.IJournal>(env.endpoints.journals);
+       return journals.map(j => this.getOptions(j.id, j.name));
+   }
+
+   public async getArticleOptions()
+   {
+       const articles = await this.getData<Models.IArticle>(env.endpoints.articles);
+       return articles.map(j => this.getOptions(j.id, j.name));
+   }
+
+   private async getData<T>(action: string): Promise<T[]>
+   {
+       const res =  await axious.get<T[]>(`${env.url}${action}`);
+       return res.data;
+   }
+
+   private getOptions = (value: number, label: string) => ({value, label});
+}
