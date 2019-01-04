791e259f070b99edf45b43765575a72ddea69a97
BMNTeam <maksim_bender08@mail.ru>
2019-01-04 18:56:35 +0300

    Types and environment has been modified

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,9 @@
 export const env = {
     client_secret: 'qcm4CBlNWwp20O2RuXYGcVdj1Pgczq2JH7PZ2xhf',
     endpoints: {
+        articles: 'api/articles',
+        employees: 'api/employees',
+        journals: 'api/journals',
         reports: 'api/reports'
     },
     url: 'http://archive.com:8888/',
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,34 @@
-interface IReport extends ObjectConstructor
+declare namespace Models
 {
-    date: string;
-    employees: number[];
-    fullReport: File;
-    manager: number;
-    name: string;
-    presentation: File;
-    shortReport: File;
-    themeNumber: string;
+    interface IArticle {
+        id: number;
+        name: string;
+        authors: string[];
+        journal: number;
+        fullText: string;
+        link: string;
+    }
+    interface IReport extends ObjectConstructor {
+        date: string;
+        employees: number[];
+        fullReport: File;
+        manager: number;
+        name: string;
+        presentation: File;
+        shortReport: File;
+        themeNumber: string;
+    }
+
+    interface IEmployee {
+        degree: string;
+        full_name: string;
+        id: number;
+        position: string;
+    }
+
+    interface IJournal {
+        name: string;
+        id: number;
+        url: string;
+    }
 }
