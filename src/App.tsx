a345b0dd35ff09cf3812f6284a72e45db251238d
BMNTeam <maksim_bender08@mail.ru>
2019-01-06 15:16:52 +0300

    Redirection after login fixed

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ class App extends React.Component {
                 <div className="main-container">
                     <Router history={history}>
                         <Switch>
-                            <Route  path="/login" component={LoginComponent}/>
+                            <Route path="/login" component={LoginComponent}/>
                             <PrivateRoute path="/" component={AppComponent}/>
                         </Switch>
 
--- src/app/shared/routes.tsx
+++ src/app/shared/routes.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import {Redirect, Route} from "react-router";
 import {ACCESS_TOKEN} from "../services/auth/auth.constants";
 
-const token = localStorage.getItem(ACCESS_TOKEN);
 export function PrivateRoute({ component: Component, ...rest } : {component: any, path: string}) { // TODO get rid of any
+    const token = localStorage.getItem(ACCESS_TOKEN);
+
     return (
         <Route
             {...rest}
