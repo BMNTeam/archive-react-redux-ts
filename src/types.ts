9ae5c4538264f33cfe8411f9258dbc16b705c801
Maksim Barsukov <barsukov@bpcbt.com>
2019-01-23 20:22:59 +0300

    Type modification

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,80 @@
 declare namespace Models
 {
-    interface IArticle {
-        id: number;
-        name: string;
-        authors: string[];
-        journal: number;
-        fullText: string;
-        link: string;
+    namespace Client {
+
+        interface IEmployee {
+            degree: string;
+            full_name: string;
+            id: number;
+            position: string;
+        }
+
+        interface IJournal {
+            name: string;
+            id: number;
+            url: string;
+        }
+        interface IArticle {
+            id: number;
+            name: string;
+            authors: string[];
+            journal: number;
+            fullText: string;
+            link: string;
+        }
+        interface INewReport extends ObjectConstructor {
+            id?: number;
+            date: string;
+            employees: number[];
+            full: File;
+            manager: number;
+            name: string;
+            presentation: File;
+            shortReport: File;
+            themeNumber: string;
+        }
     }
-    interface IReport extends ObjectConstructor {
-        id?: number;
-        date: string;
-        employees: number[];
-        fullReport: File;
-        manager: number;
-        name: string;
-        presentation: File;
-        shortReport: File;
-        themeNumber: string;
+    
+
+    namespace Server {
+        interface IReport {
+            articles: IArticle[];
+            date: string;
+            employees: IEmployee[];
+            id: string;
+            full_report_text: string;
+            full_report_url: string;
+            manager_id: string;
+            name: string;
+            presentation_url: string;
+            short_report_text: string;
+            short_report_url: string;
+            theme_number: string;
+        }
+        
+        interface IEmployee {
+            id: number;
+            degree: string;
+            full_name: string;
+            position: string;
+        }
+
+        interface IArticle {
+            id: number;
+            full_text: string;
+            journal_id: number;
+            link: string;
+            name: string;
+        }
+
     }
 
-    interface IEmployee {
-        degree: string;
-        full_name: string;
-        id: number;
-        position: string;
-    }
-
-    interface IJournal {
-        name: string;
-        id: number;
-        url: string;
-    }
+    
 }
 
 declare namespace Search
 {
-    import IEmployee = Models.IEmployee;
+    import IEmployee = Models.Client.IEmployee;
 
     interface ISearchRequest {
         value: string;
